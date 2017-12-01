b7226d1e427e5b4e6f98cd9786e7924c50c8d492
hollygregory <32820231+hollygregory@users.noreply.github.com>
2017-12-01 16:30:03 +0000

    Capitalised 'coding groups'

---
+++ js.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+	$('.show').hide();
+	$('.show').children().hide();
+	$('input:checkbox').change(function(){
+		if ($(this).is(':checked')) {
+			if ($(this).val() == 'show') {
+				$('.show').hide();
+			}
+		}
+	})
+	$('.info').hide();
+	$('.show').on('click', function(){
+		var list = $(this).find('.info');
+		if ( $(list).css('display') == 'none') {
+			$(list).show();
+		} else {
+			$(list).hide();
+		}
+	});
+
+	$('input:checkbox').change(function(){
+		$('.show').hide();
+		if ($(this).is(':checked')) {
+			var val = '.'+ $(this).val();
+			$(val).parent().show()
+			$(val).children().show()
+		}
+	});	
+	$('input:checkbox').change(function(){	 
+		if (!$(this).is(':checked')) {
+			var val = '.'+ $(this).val();
+			$(val).children().hide();
+		}
+	});
+})
